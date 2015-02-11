318789fb01ec094e01c08de5d54404821748fe2b
howard <howardfk@gmail.com>
2015-02-11 14:34:01 -0500

    initla empty file

---
+++ signal_flat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+#read in file line by line
+#store file into array
+
+#pick out time stamps pick (fix time stamps)
+
+#pull in power into 2D array
+
+#find the singal
+
+#modify data
+
+
+#optamize
