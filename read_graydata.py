c3058f9c0c524ac34ea1dc6dea68dcff08bbd466
howard <howardfk@gmail.com>
2015-02-11 14:33:23 -0500

    added a basic working directory for file

---
+++ read_graydata.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import os
+import sys
+import numpy as np
+
+#creat path to read file
+def make_path(dir_name,file_name=""):
+   script_dir = os.path.abspath(__file__)
+   script_dir = os.path.dirname(script_dir) #gives you back this files abs path to dir - filename
+   #I will temperatly store my graydata to be anlyise here
+
+#read in line by line
+
+
+#Store times into array
+
+#Store power spectra into array
+
+#Combine arrays
+
+#check the data
