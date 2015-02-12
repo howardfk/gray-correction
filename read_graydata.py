e38208884a6945ca29d5f308a4ff50dc3b212d1b
howard <howardfk@gmail.com>
2015-02-12 01:00:52 -0500

    not much changed

--- read_graydata.py
+++ read_graydata.py
@@ -4,10 +4,15 @@ import sys
 import numpy as np
 
 #creat path to read file
-def make_path(dir_name,file_name=""):
+def make_path():
    script_dir = os.path.abspath(__file__)
+   print script_dir
    script_dir = os.path.dirname(script_dir) #gives you back this files abs path to dir - filename
+   return script_dir
    #I will temperatly store my graydata to be anlyise here
+
+def pathtofile(file_name=""):
+    print make_path()
 
 #read in line by line
 
