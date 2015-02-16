ac87243860ee4eba19cf7fb0dd2b56d21fb34870
howard <howardfk@gmail.com>
2015-02-16 13:51:44 -0500

    I have added an acutle path to a file name in directory data.  Has an error, data is not a proper file path

--- read_graydata.py
+++ read_graydata.py
@@ -4,17 +4,28 @@ import sys
 import numpy as np
 
 #creat path to read file
-def make_path():
+def make_path(file_name):
    script_dir = os.path.abspath(__file__)
    script_dir = os.path.dirname(script_dir) #gives you back this files abs path to dir - filename
+   script_dir = os.path.join(script_dir,data/file_name)
    return script_dir
    #I will temperatly store my graydata to be anlyise here
 
 def pathtofile(file_name=""):
-    print(make_path())
+    return make_path(file_name)
+
+#length of entire file
+def howlongisfile(file_in):
+   with open(file_in, r) as f:
+      for i in enumerate(f,1):
+         pass
+      else:
+         return i[0]
 
 #read in line by line
-
+def filetoarray(file_in):
+   filelength = howlongisfile(file_in)
+   data_array[0]*filelength
 
 #Store times into array
 
@@ -22,13 +33,14 @@ def pathtofile(file_name=""):
 
 #Combine arrays
 
+
 #check the data
 
 
 #runing code
 
 def main():
-   pathtofile()
+   pathtofile("20140611-223001-SPS.data.2.5MHz.data.021925.avg4096.graydata")
 
 if __name__ == "__main__":
    main()
