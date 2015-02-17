cbde5ecc83f7af08241bc93bbdf36e0eb178936d
howard <howardfk@gmail.com>
2015-02-16 21:32:43 -0500

    read in entire graydata file and line-by-line incerted each value into a single 1-D matrix, seems to read values correctly

--- read_graydata.py
+++ read_graydata.py
@@ -7,40 +7,52 @@ import numpy as np
 def make_path(file_name):
    script_dir = os.path.abspath(__file__)
    script_dir = os.path.dirname(script_dir) #gives you back this files abs path to dir - filename
-   script_dir = os.path.join(script_dir,data/file_name)
+   script_dir = os.path.join(script_dir,'data', file_name)
    return script_dir
    #I will temperatly store my graydata to be anlyise here
 
-def pathtofile(file_name=""):
-    return make_path(file_name)
+def pathtodata(file_name=''):
+   return make_path(file_name)
 
 #length of entire file
 def howlongisfile(file_in):
-   with open(file_in, r) as f:
+   with open(file_in, 'r') as f:
       for i in enumerate(f,1):
          pass
       else:
          return i[0]
 
 #read in line by line
-def filetoarray(file_in):
-   filelength = howlongisfile(file_in)
-   data_array[0]*filelength
+def datatoarray(pathToData, sampleFreq, binNum, fftAvg):
+   filelength = howlongisfile(pathToData)
+   data_array = np.zeros(filelength)
+   print data_array.size
+   with open(pathToData,'r') as f:
+      data_array = f.readlines()
+   print 'first ', data_array[0]
+   print 'second ', data_array[1]
+   return data_array
+
+def converttomatrix(pathToData, sampleFreq, binNum, fftAvg):
+   data_array = datatoarray(pathToData, sampleFreq, binNum, fftAvg)
+   datatoarray(pathToData, sampleFreq, binNum, fftAvg)
 
 #Store times into array
-
 #Store power spectra into array
-
 #Combine arrays
-
-
 #check the data
 
 
 #runing code
-
 def main():
-   pathtofile("20140611-223001-SPS.data.2.5MHz.data.021925.avg4096.graydata")
+   sampleFreq = raw_input("Sampiling Freuancy:")
+   binNum = raw_input("Number of Bins for the FFT: ")
+   fftAvg = raw_input("Number of averging for FFT: ")
+   cwd=pathtodata("20140611-223001-SPS.data.2.5MHz.data.021925.avg4096.graydata")
+   data_array = datatoarray(cwd, sampleFreq, binNum, fftAvg)
+   i=0
+   for i in range(0,5):
+      print data_array[i*2]
 
 if __name__ == "__main__":
    main()
