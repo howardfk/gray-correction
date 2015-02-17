6661380efe09c12d1bff2b2a175be0c6aad80aaa
howard <howardfk@gmail.com>
2015-02-17 11:24:41 -0500

    added some sudo code def find_signal, def signal_flat

--- signal_flat.py
+++ signal_flat.py
@@ -6,6 +6,21 @@
 #FFT bins = N-bins/2 
 #find the singal  FreqRez = Fsample / FFT samples    TimeRez = (FFTs/Fs)*averages
 
+
+def find_signal(ffts, freqs, signal = 100000, binindex ="")
+   if binindex =="":
+      freqrez = freqs/ffts
+      #write something to check for the correct bin
+      #for now we can hard code it
+      return binindex
+   else:
+      return binindex 
+
+def flatten(corection, spec_inx):
+   #loop data over one spectra
+   #2dmatri[i][j]=2dmatrix[i][j]+correction
+   #not sure if spec_inx will be collom or row
+
 #modify data
 
 
